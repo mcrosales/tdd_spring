37ac9b28350b8f83031b8332213e25874cac6746
mario_rosales <mariocrosl@gmail.com>
2019-12-28 14:21:38 +0100

    first commit

---
+++ src/test/java/com/tdd/tdd_demo/TddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.tdd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
